ebffcf96aad28fa7e7f4a74cd84e653557150d7b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-26 01:44:14 +0100

    modified little things

--- lib/wp_api.dart
+++ lib/wp_api.dart
@@ -5,15 +5,9 @@ import 'dart:convert';
 import 'package:wp_blog_app/models/posts.dart';
 
 class WpApi {
-
-  static const API = "https://www.naijaloaded.com.ng/wp-json/wp/v2/posts?_embed";
-  static const headers = {
-    "Accept": "application/json"
-  };
-  // static const API = "https://www.naijatechguy.com/wp-json/wp/v2/posts?_embed";
-  // static const headers = {
-  //   "Accept": "application/json"
-  // };
+  static const API =
+      "https://www.naijaloaded.com.ng/wp-json/wp/v2/posts";
+  static const headers = {"Accept": "application/json"};
 
   Future<List<Posts>> fetchTopPosts() async {
     final response = await http.get(
@@ -21,26 +15,29 @@ class WpApi {
       headers: headers,
     );
 
-    var convertDataToJson = jsonDecode(response.body);
+    var convertDataToJson = json.decode(response.body);
+
     List<Posts> posts = List();
 
     convertDataToJson.forEach((post) {
-
       String title = post['title']['rendered'];
+
       if (title.length > 30) {
-        title = post['title']['rendered'].substring(1, 30) + "...";
+        title = post['title']['rendered'].substring(1, 20) + "...";
       }
+
       var imageUrl = post['_embedded']['wp:featuredmedia'] != null
-                            ? post['_embedded']['wp:featuredmedia'][0]
-                                ['source_url']
-                            : Text("Error");
-      // var image = post['url'];
-      // String image = post['url'];
-      posts.add(Posts(title: title, image: imageUrl));
-      
+          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+          : Container(
+            width: 100,
+            height: 100,
+            child: Image.network(
+                'assets/images/img_error.jpg',
+              ),
+          );
+      var time = post['date'];
+      posts.add(Posts(title: title, image: imageUrl, time: time));
     });
-
-
     return posts;
   }
 
@@ -54,22 +51,16 @@ class WpApi {
     List<Posts> posts = List();
 
     convertDataToJson.forEach((post) {
-
       String title = post['title']['rendered'];
       if (title.length > 30) {
         title = post['title']['rendered'].substring(1, 30) + "...";
       }
       var imageUrl = post['_embedded']['wp:featuredmedia'] != null
-                            ? post['_embedded']['wp:featuredmedia'][0]
-                                ['source_url']
-                            : Text("Error");
-      // var image = post['url'];
-      // String image = post['url'];
+          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+          : Text("Error");
+
       posts.add(Posts(title: title, image: imageUrl));
-
     });
-
-
     return posts;
   }
 }
