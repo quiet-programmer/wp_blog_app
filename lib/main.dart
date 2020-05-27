710feb8a4cf5e77f81ab58dc527a2f7790ece7ef
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-27 16:49:34 +0100

    added state root

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wp_blog_app/providers/brightness_provider.dart';
 import 'package:wp_blog_app/src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: BrightnessProvider(),
+        )
+      ],
+      child: App(),
+    ),
+  );
+}
