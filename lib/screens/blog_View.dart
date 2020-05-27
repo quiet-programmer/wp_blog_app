68c844782a540314a14ca276b82e55fb10a33d98
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-27 16:51:17 +0100

    little changes

--- lib/screens/blog_View.dart
+++ lib/screens/blog_View.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wp_blog_app/widgets/horizonatl_view.dart';
-import 'package:wp_blog_app/widgets/list_view_post.dart';
 
 class BlogView extends StatefulWidget {
   @override
@@ -10,34 +8,11 @@ class BlogView extends StatefulWidget {
 class _BlogViewState extends State<BlogView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 40.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           
-              Container(
-                margin: EdgeInsets.only(left: 10.0, bottom: 10.0),
-                child: Text(
-                  "You may like",
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-          HorizontalView(),
-          Container(
-            margin: EdgeInsets.only(left: 10.0),
-            child: Text(
-              "Your Choice",
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          ListViewPost(),
         ],
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
-import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:wp_blog_app/screens/blog_View.dart';
-import 'package:wp_blog_app/screens/bookmark.dart';
-import 'package:wp_blog_app/screens/category.dart';
-import 'package:wp_blog_app/screens/settings.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:wp_blog_app/providers/brightness_provider.dart';
+import 'package:wp_blog_app/widgets/horizonatl_view.dart';
+import 'package:wp_blog_app/widgets/list_view_post.dart';
+
+import '../const_values.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,54 +13,98 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<Map<String, Object>> _pages = [
-    {
-      'page': BlogView(),
-      'title': 'Home',
-    },
-    {
-      'page': Bookmark(),
-      'title': 'Bookmark',
-    },
-    {
-      'page': Category(),
-      'title': 'Categories',
-    },
-    {
-      'page': Settings(),
-      'title': 'Settings',
-    },
-  ];
-
-  int _selectedPageIndex = 0;
-
-  void _selectPage(int index) {
-    setState(() {
-      _selectedPageIndex = index;
-    });
-  }
-
+  var dateFormat = DateFormat.yMMMMEEEEd().format(DateTime.now());
   @override
   Widget build(BuildContext context) {
+    final changeData = Provider.of<BrightnessProvider>(context);
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(_pages[_selectedPageIndex]['title']),
-      //   actions: <Widget>[
-      //   ],
-      // ),
-      body: _pages[_selectedPageIndex]['page'],
-      bottomNavigationBar: FancyBottomNavigation(
-        tabs: [
-          TabData(iconData: Icons.home, title: "Home"),
-          TabData(iconData: Icons.bookmark, title: "Bookmark"),
-          TabData(iconData: Icons.list, title: "Categories"),
-          TabData(iconData: Icons.settings, title: "Settings"),
+      backgroundColor: changeData.isDark == false ? mainColor : darkColorTwo,
+      appBar: AppBar(
+        leading: Icon(Icons.format_align_left),
+        actions: <Widget>[
+          Icon(Icons.search),
+          IconButton(
+            icon: Icon(
+                changeData.isDark ? Icons.brightness_6 : Icons.brightness_3),
+            onPressed: () {
+              changeData.changeTheme();
+            },
+          )
         ],
-
-        onTabChangedListener: (index) {
-          _selectPage(index);
-        },
-
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 290,
+                decoration: BoxDecoration(
+                  color: changeData.isDark == false ? Colors.white : darkColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(33),
+                    bottomRight: Radius.circular(33),
+                  ),
+                ),
+                child: Container(
+                  height: 290,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                "Popular",
+                                style: TextStyle(
+                                  color: changeData.isDark == false ? Colors.black : Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                              Text(
+                                "$dateFormat",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      HorizontalView(),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 40.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Recent Articles",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              ListViewPost(),
+            ],
+          ),
+        ),
       ),
     );
   }
