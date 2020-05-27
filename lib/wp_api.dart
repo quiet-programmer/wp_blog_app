7333f38da4d4859adb4bb2aa3c26f9428e9ccd14
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-27 16:48:03 +0100

    little changes

--- lib/wp_api.dart
+++ lib/wp_api.dart
@@ -5,45 +5,44 @@ import 'dart:convert';
 import 'package:wp_blog_app/models/posts.dart';
 
 class WpApi {
-  static const API =
-      "https://www.naijaloaded.com.ng/wp-json/wp/v2/posts";
+  static const api = "https://www.naijaloaded.com.ng/wp-json/wp/v2/posts";
   static const headers = {"Accept": "application/json"};
 
   Future<List<Posts>> fetchTopPosts() async {
     final response = await http.get(
-      API,
+      api,
       headers: headers,
     );
+    List<Posts> posts = List();
 
     var convertDataToJson = json.decode(response.body);
 
-    List<Posts> posts = List();
+      convertDataToJson.forEach((post) {
+        String title = post['title']['rendered'];
 
-    convertDataToJson.forEach((post) {
-      String title = post['title']['rendered'];
+        if (title.length > 30) {
+          title = post['title']['rendered'].substring(1, 20) + "...";
+        }
 
-      if (title.length > 30) {
-        title = post['title']['rendered'].substring(1, 20) + "...";
-      }
+        var imageUrl = post['_embedded']['wp:featuredmedia'] != null
+            ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+            : Container(
+                width: 100,
+                height: 100,
+                child: Image.network(
+                  'assets/images/img_error.jpg',
+                ),
+              );
+        var time = post['date'];
+        posts.add(Posts(title: title, image: imageUrl, time: time));
+      });
 
-      var imageUrl = post['_embedded']['wp:featuredmedia'] != null
-          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
-          : Container(
-            width: 100,
-            height: 100,
-            child: Image.network(
-                'assets/images/img_error.jpg',
-              ),
-          );
-      var time = post['date'];
-      posts.add(Posts(title: title, image: imageUrl, time: time));
-    });
     return posts;
   }
 
   Future<List<Posts>> fetchListPosts() async {
     final response = await http.get(
-      API,
+      api,
       headers: headers,
     );
 
