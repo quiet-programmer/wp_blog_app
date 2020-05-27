ec6c0ff3cd104f1af676ceea9926a11db51bb682
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-27 16:50:03 +0100

    added new values

--- lib/const_values.dart
+++ lib/const_values.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
 
+//primary colors
 const mainColor = Color.fromRGBO(97, 44, 88, 1);
 const subColor = Color.fromRGBO(255, 133, 0, 1);
+//
+
+// dark and light theme color
+const darkColor = Color.fromRGBO(32,32,64, 1);
+const darkColorTwo = Color.fromRGBO(32,32,96, 1);
+//
