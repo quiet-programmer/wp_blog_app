a0e223a6eeb011341460de528b49b480ec82859f
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-30 12:02:44 +0100

    trying to fix state of dark mode

--- lib/providers/brightness_provider.dart
+++ lib/providers/brightness_provider.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BrightnessProvider with ChangeNotifier {
   bool isDark = false;
 
-  void changeTheme() {
-    isDark = !isDark;
+  Future<bool> changeTheme() async {
+    notifyListeners();
+    return isDark = !isDark;
+  }
+
+  //store the state
+  void saveState() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    var getIsDark = (pref.getBool('isDark') ?? changeTheme());
+    await pref.setBool('isDark', getIsDark);
     notifyListeners();
   }
 }
