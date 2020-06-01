cdc99d1b00be8d76c641f85d19189c55aaa7baf7
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-01 14:52:29 +0100

    little changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -102,7 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: 15.0,
               ),
-              ListViewPost(),
+              // ListViewPost(),
             ],
           ),
         ),
