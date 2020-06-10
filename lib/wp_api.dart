ac295ed15448da03bab04f529cd8148f0268733a
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-10 22:38:13 +0100

    fixing fetch issues

--- lib/wp_api.dart
+++ lib/wp_api.dart
@@ -6,18 +6,19 @@ import 'package:wp_blog_app/models/posts.dart';
 
 class WpApi {
 
-  static const api = "https://www.naijaloaded.com.ng/wp-json/wp/v2/posts?_embedded";
+  static const api = "https://www.naijaloaded.com.ng/wp-json/wp/v2/";
   static const headers = {"Accept": "application/json"};
-  List<Posts> _posts = List();
 
-  Future<List<Posts>> _fetchTopPosts() async {
+  Future<List<Posts>> fetchTopPosts() async {
 
     var response = await http.get(
-      api,
+      Uri.encodeFull(api + "posts?_embed"),
       headers: headers,
     );
 
     var convertDataToJson = jsonDecode(response.body);
+
+    List<Posts> posts = [];
 
     convertDataToJson.forEach((post) {
       String title = post['title']['rendered'];
@@ -27,27 +28,27 @@ class WpApi {
       //   print(title);
       // }
 
-      var imageUrl = post['_embedded']['wp:featuredmedia'] != null
-          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
-          : Image.network(
-              'assets/images/img_error.jpg',
-            );
+      // var imageUrl = post['_embedded']['wp:featuredmedia'] != null
+      //     ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+      //     : Image.network(
+      //         'assets/images/img_error.jpg',
+      //       );
 
-            print(imageUrl.toString());
+      //       print(imageUrl.toString());
 
       var time = post['date'];
       print(time.toString());
 
-      _posts.add(Posts(title: title, image: imageUrl, time: time));
+      posts.add(Posts(title: title, time: time));
 
     });
 
-    return _posts;
+    return posts;
   }
 
-  Future<List<Posts>> get topPost {
-    return _fetchTopPosts();
-  }
+  // Future<List<Posts>> get topPost {
+  //   return _fetchTopPosts();
+  // }
 
   Future<List<Posts>> fetchListPosts() async {
     final response = await http.get(
