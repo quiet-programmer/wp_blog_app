7355fcb5f6126f8d63b9c992547de3122c1e2e2f
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-23 16:13:08 +0100

    configured Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:wp_blog_app/const_values.dart';
 import 'package:wp_blog_app/providers/brightness_provider.dart';
 import 'package:wp_blog_app/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory document = await getApplicationDocumentsDirectory();
+  Hive.registerAdapter(BrightnessProviderAdapter());
+  Hive.init(document.path);
+  final stateOfApp = await Hive.openBox(appState);
+  if (stateOfApp.get('state') == null) {
+    stateOfApp.put("state", BrightnessProvider());
+  }
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: BrightnessProvider(),
-        ),
-      ],
-      child: App(),
+    WatchBoxBuilder(
+      box: Hive.box(appState),
+      builder: (_, box) => App(),
     ),
   );
 }
