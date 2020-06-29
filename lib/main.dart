ea565394b14ee6a20bc6ea9e3483c3e187ca4693
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-29 22:02:55 +0100

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,17 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:wp_blog_app/const_values.dart';
-import 'package:wp_blog_app/providers/brightness_provider.dart';
+import 'package:wp_blog_app/models/posts.dart';
 import 'package:wp_blog_app/src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Directory document = await getApplicationDocumentsDirectory();
-  Hive.registerAdapter(BrightnessProviderAdapter());
+  Hive.registerAdapter(PostsAdapter());
   Hive.init(document.path);
   final stateOfApp = await Hive.openBox(appState);
   if (stateOfApp.get('state') == null) {
-    stateOfApp.put("state", BrightnessProvider());
+    stateOfApp.put("state", Posts());
   }
   runApp(
     WatchBoxBuilder(
