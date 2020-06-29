e0688dbb4e65ccbda6f21619e3b4b8fadb61d181
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-29 22:04:15 +0100

    config models for hive

--- lib/models/posts.dart
+++ lib/models/posts.dart
@@ -1,12 +1,31 @@
-import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
 
+part 'posts.g.dart';
+
+@HiveType(typeId: 0)
 class Posts {
 
+  @HiveField(0)
+  bool isDark = false;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final String image;
+
+  @HiveField(3)
   final String contents;
+
+  @HiveField(4)
   final String time;
+
+  @HiveField(5)
   final String authur;
 
-  Posts({@required this.title, this.image, this.contents, this.time, this.authur});
+  Posts({this.title, this.image, this.contents, this.time, this.authur});
+
+  void changeTheme()  {
+    isDark = !isDark;
+  }
 }
