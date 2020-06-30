7adaf1f421603499022e471eb46ba513b157a4c7
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-30 18:35:35 +0100

    added constant files

--- lib/const_values.dart
+++ lib/const_values.dart
@@ -10,5 +10,9 @@ const darkColor = Color.fromRGBO(32,32,64, 1);
 const darkColorTwo = Color.fromRGBO(32,32,96, 1);
 //
 
+//other colors
+const defaultWhite = Color.fromRGBO(255,255,255, 1);
+const defaultBlack = Color.fromRGBO(0,0,0, 1);
+
 // hive box name
 const String appState = "appstate";
