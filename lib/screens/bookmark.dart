8d0f02b303a301742029277098edb2bb25242063
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-07-07 22:01:20 +0100

    config bookmark screen

--- lib/screens/bookmark.dart
+++ lib/screens/bookmark.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:wp_blog_app/const_values.dart';
+import 'package:wp_blog_app/models/posts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Bookmark extends StatefulWidget {
   @override
@@ -8,10 +12,23 @@ class Bookmark extends StatefulWidget {
 class _BookmarkState extends State<Bookmark> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("No Bookmark Yet!"),
-      ),
+    final Posts changeData = Hive.box(appState).get('state');
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(appState).listenable(),
+      builder: (context, box, _) {
+        return Scaffold(
+          backgroundColor:
+              changeData.isDark == false ? defaultWhite : darkColor,
+          body: Center(
+            child: Text(
+              "No Bookbark Yet!",
+              style: TextStyle(
+                color: changeData.isDark == false ? defaultBlack : defaultWhite,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
