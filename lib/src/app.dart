ffd43c31137343eb3e73b72b35f3bb59b2c68748
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-07-20 19:28:21 +0100

    fixed theme change issues

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:wp_blog_app/providers/brightness_provider.dart';
-import 'package:wp_blog_app/screens/home_screen.dart';
+import 'package:wp_blog_app/models/posts.dart';
+import 'package:wp_blog_app/screens/tab_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../const_values.dart';
 
@@ -11,30 +12,34 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-
   @override
   Widget build(BuildContext context) {
     // final changeData = Provider.of<BrightnessProvider>(context);
-    final BrightnessProvider changeData = Hive.box(appState).get('state');
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WP Flutter Blog Demo',
-      theme: ThemeData(
-        primaryColor: mainColor,
-        accentColor: subColor,
-        appBarTheme: AppBarTheme(
-          brightness: Brightness.dark,
-          elevation: 0.0,
-          color: changeData.isDark == false ? Colors.white : darkColor,
-          iconTheme: IconThemeData(
-            color: changeData.isDark == false ? Colors.black : Colors.white,
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(appState).listenable(),
+      builder: (context, box, _) {
+        final Posts changeData = Hive.box(appState).get('state');
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'WP Flutter Blog Demo',
+          theme: ThemeData(
+            primaryColor: mainColor,
+            accentColor: subColor,
+            appBarTheme: AppBarTheme(
+              brightness: Brightness.dark,
+              elevation: 0.0,
+              color: changeData.isDark == false ? Colors.white : darkColor,
+              iconTheme: IconThemeData(
+                color: changeData.isDark == false ? Colors.black : Colors.white,
+              ),
+              textTheme: TextTheme(
+                headline6: TextStyle(color: Colors.black),
+              ),
+            ),
           ),
-          textTheme: TextTheme(
-            headline6: TextStyle(color: Colors.black),
-          ),
-        ),
-      ),
-      home: HomeScreen(),
+          home: TabView(),
+        );
+      },
     );
   }
 }
