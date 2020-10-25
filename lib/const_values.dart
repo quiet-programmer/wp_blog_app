c1c93e38b98590859150091c32b12ae0f3a4af7b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-25 20:19:12 +0100

    added functions to calculate text and container size

--- lib/const_values.dart
+++ lib/const_values.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wp_blog_app/size_config.dart';
 
 //primary colors
 const mainColor = Color.fromRGBO(97, 44, 88, 1);
@@ -6,13 +7,36 @@ const subColor = Color.fromRGBO(255, 133, 0, 1);
 //
 
 // dark and light theme color
-const darkColor = Color.fromRGBO(32,32,64, 1);
-const darkColorTwo = Color.fromRGBO(32,32,96, 1);
+Color darkColor = Colors.grey[900];
+const darkColorTwo = Color.fromRGBO(32, 32, 96, 1);
+
+Color cardColor = Colors.grey[850];
 //
 
+setTextSize(double size) {
+  var textMultiplier = SizeConfig.textMultiplier;
+  var textHeight = size / SizeConfig.heightMultiplier;
+  var textProperSize = textHeight * textMultiplier;
+  return textProperSize;
+}
+
+setContainerHeight(double size) {
+  var containerMultiplier = SizeConfig.heightMultiplier;
+  var myHeight = size / SizeConfig.heightMultiplier;
+  var properHeight = myHeight * containerMultiplier;
+  return properHeight;
+}
+
+setContainerWidth(double size) {
+  var containerMultiplier = SizeConfig.widthMultiplier;
+  var myHeight = size / SizeConfig.widthMultiplier;
+  var properHeight = myHeight * containerMultiplier;
+  return properHeight;
+}
+
 //other colors
-const defaultWhite = Color.fromRGBO(255,255,255, 1);
-const defaultBlack = Color.fromRGBO(0,0,0, 1);
+const defaultWhite = Color.fromRGBO(255, 255, 255, 1);
+const defaultBlack = Color.fromRGBO(0, 0, 0, 1);
 
 // hive box name
 const String appState = "appstate";
