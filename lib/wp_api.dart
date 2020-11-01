f84f721f9feab0bb0020e616d66445e2360017af
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-11-01 08:56:10 +0100

    little fix for api calls

--- lib/wp_api.dart
+++ lib/wp_api.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 import 'package:wp_blog_app/models/posts.dart';
 
 class WpApi {
-  static const api = "https://www.naijatechguy.com/wp-json/wp/v2/";
+  static const api = "https://www.naijatechguy.com/wp-json/wp/v2";
   static const listApi = "https://www.naijatechguy.com/wp-json/wp/v2/";
   static const headers = {"Accept": "application/json"};
 
@@ -19,35 +19,40 @@ class WpApi {
   }
 
   Future<List<Posts>> fetchTopPosts() async {
-    var response = await http.get(
-      Uri.encodeFull(api + "posts?_embed&categories=417"),
-      headers: headers,
-    );
-
-    var convertDataToJson = jsonDecode(response.body);
-
     List<Posts> posts = [];
+    try {
+      var response = await http.get(
+        Uri.encodeFull("$api/posts?_embed&per_page=20"),
+        headers: headers,
+      );
 
-    convertDataToJson.forEach((post) {
-      String title = _parseHtmlString(post['title']['rendered']);
+      var convertDataToJson = json.decode(response.body);
+      convertDataToJson.forEach((post) {
+        String title = _parseHtmlString(post['title']['rendered']);
 
-      // if (title.length > 30) {
-      //   title = _parseHtmlString(post['title']['rendered']).substring(0, 20) + "...";
-      // }
+        // if (title.length > 30) {
+        //   title = _parseHtmlString(post['title']['rendered']).substring(0, 20) + "...";
+        // }
 
-      var time = post['date'];
+        var time = post['date'];
 
-      var content = _parseHtmlString(post['content']['rendered']);
+        var content = _parseHtmlString(post['content']['rendered']);
 
-      var imageUrl = post['_embedded']['wp:featuredmedia'] != null
-          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
-          : Image.network(
-              'assets/images/img_error.jpg',
-            );
+        var imageUrl = post['_embedded']['wp:featuredmedia'] != null
+            ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+            : '';
 
-      posts.add(
-          Posts(title: title, image: imageUrl, contents: content, time: time));
-    });
+        posts.add(Posts(
+          title: title,
+          image: imageUrl,
+          contents: content,
+          time: time,
+        ));
+      });
+    } catch (e) {
+      print(e.toString());
+      throw (e);
+    }
 
     return posts;
   }
@@ -66,9 +71,38 @@ class WpApi {
     convertDataToJson.forEach((post) {
       String title = _parseHtmlString(post['title']['rendered']);
 
-      // if (title.length > 30) {
-      //   title = _parseHtmlString(post['title']['rendered']).substring(0, 50) + "...";
-      // }
+      var content = _parseHtmlString(post['content']['rendered']);
+      var time = post['date'];
+
+      var imageUrl = post['_embedded']['wp:featuredmedia'] != null
+          ? post['_embedded']['wp:featuredmedia'][0]['source_url']
+          : Image.network(
+              'assets/images/img_error.jpg',
+              fit: BoxFit.cover,
+              width: 100,
+              height: 90,
+            );
+
+      posts.add(
+          Posts(title: title, image: imageUrl, contents: content, time: time));
+    });
+
+    return posts;
+  }
+
+  // api call for categories sections
+  Future<List<Posts>> fetchOtherCategories(int cartCode) async {
+    var response = await http.get(
+      Uri.encodeFull(listApi + "posts?_embed&categories=$cartCode"),
+      headers: headers,
+    );
+
+    var convertDataToJson = jsonDecode(response.body);
+
+    List<Posts> posts = [];
+
+    convertDataToJson.forEach((post) {
+      String title = _parseHtmlString(post['title']['rendered']);
 
       var content = _parseHtmlString(post['content']['rendered']);
       var time = post['date'];
