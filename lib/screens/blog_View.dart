38d689d09df954a62c587d021544f706e8e7dbbd
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-02-23 00:49:32 +0100

    blog view for bookmarked articles

--- lib/screens/blog_View.dart
+++ lib/screens/blog_View.dart
@@ -1,19 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+import 'package:wp_blog_app/const_values.dart';
+import 'package:wp_blog_app/models/posts.dart';
 
 class BlogView extends StatefulWidget {
+  final Posts index;
+
+  BlogView({this.index});
   @override
   _BlogViewState createState() => _BlogViewState();
 }
 
 class _BlogViewState extends State<BlogView> {
+  displayTime(String date) {
+    return DateFormat.yMMMMEEEEd().format(DateTime.parse(date));
+  }
+
+  Box storeData;
+
+  @override
+  void initState() {
+    super.initState();
+    storeData = Hive.box(appState);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Column(
-        children: <Widget>[
-          
-        ],
+    final Posts changeData = storeData.get('state');
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: setContainerHeight(300),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(33),
+                      bottomRight: Radius.circular(33),
+                    ),
+                    image: DecorationImage(
+                        image: NetworkImage(widget.index.image),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    widget.index.title,
+                    style: TextStyle(
+                      color: changeData.isDark == false
+                          ? Colors.black
+                          : Colors.white,
+                      fontSize: setTextSize(23),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "${displayTime(widget.index.time)}",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    widget.index.contents,
+                    style: TextStyle(
+                      color: changeData.isDark == false
+                          ? Colors.black
+                          : Colors.white,
+                      fontSize: setTextSize(18),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
