db9f87464a3b84f81ae4a7f919fe8ed78019d7ef
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-09 07:04:16 +0100

    little changes

--- lib/custom_theme.dart
+++ lib/custom_theme.dart
@@ -5,10 +5,19 @@ ThemeData buildLightTheme() => ThemeData.light().copyWith(
       cardColor: Colors.white,
       backgroundColor: Colors.grey[100],
       accentColor: Colors.grey[800],
+      scaffoldBackgroundColor: Colors.white,
       appBarTheme: AppBarTheme(
+        centerTitle: true,
         brightness: Brightness.dark,
         elevation: 0.0,
         color: defaultWhite,
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
         iconTheme: IconThemeData(
           color: Colors.grey[900],
         ),
@@ -20,9 +29,18 @@ ThemeData buildDarkTheme() => ThemeData.dark().copyWith(
       cardColor: Colors.grey[850],
       backgroundColor: Colors.grey[900],
       accentColor: Colors.grey[400],
+      scaffoldBackgroundColor: darkColor,
       appBarTheme: AppBarTheme(
+        centerTitle: true,
         brightness: Brightness.dark,
         elevation: 0.0,
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
         color: Colors.grey[900],
         iconTheme: IconThemeData(
           color: Colors.grey[400],
