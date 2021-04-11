9cc770083b5417f0b73a430d3ead30943069cdb3
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-11 09:42:06 +0100

    removed unwanted code

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:provider/provider.dart';
 import 'package:wp_blog_app/custom_theme.dart';
-import 'package:wp_blog_app/models/posts.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:wp_blog_app/providers/theme_provider.dart';
 import 'package:wp_blog_app/size_config.dart';
 import 'package:wp_blog_app/src/splash.dart';
-
-import '../const_values.dart';
 
 class App extends StatefulWidget {
   @override
@@ -16,25 +13,19 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    // final changeData = Provider.of<BrightnessProvider>(context);
-    final Posts changeData = Hive.box(appState).get('state');
-    return ValueListenableBuilder(
-      valueListenable: Hive.box(appState).listenable(),
-      builder: (context, box, _) {
-        return LayoutBuilder(
-          builder: (_, constraints) {
-            return OrientationBuilder(
-              builder: (_, orientation) {
-                SizeConfig().init(constraints, orientation);
-                return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  title: 'NaijaTechGuy Blog',
-                  theme: changeData.isDark == false
-                      ? buildLightTheme()
-                      : buildDarkTheme(),
-                  home: SplashScreen(),
-                );
-              },
+    final isThemeChange = Provider.of<ThemeProvider>(context);
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        return OrientationBuilder(
+          builder: (_, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'NaijaTechGuy Blog',
+              theme: isThemeChange.mTheme == false
+                  ? buildLightTheme()
+                  : buildDarkTheme(),
+              home: SplashScreen(),
             );
           },
         );
