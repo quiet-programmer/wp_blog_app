5ad1a3139d5d7edcf5dab9a2f065d03ccd0edcea
alibildir <84903907+alibildir@users.noreply.github.com>
2022-05-25 22:52:48 +0300

    Update wp_api.dart

--- lib/wp_api.dart
+++ lib/wp_api.dart
@@ -6,8 +6,8 @@ import 'dart:convert';
 import 'package:wp_blog_app/models/posts.dart';
 
 class WpApi {
-  static const api = "https://www.aydinlilar.org/wp-json/wp/v2";
-  static const listApi = "https://www.aydinlilar.org/wp-json/wp/v2/";
+  static const api = "https://www.naijatechguy.com/wp-json/wp/v2/";
+  static const listApi = "https://www.naijatechguy.com/wp-json/wp/v2/";
   static const headers = {"Accept": "application/json"};
 
   String _parseHtmlString(String htmlString) {
